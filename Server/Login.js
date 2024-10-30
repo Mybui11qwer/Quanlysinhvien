fac47e640f6d1790dc3cba7a544c021ae7b25b08
Mybui11qwer <145487630+Mybui11qwer@users.noreply.github.com>
2024-10-31 00:49:58 +0700

    connect Mongodb compass

--- Server/Login.js
+++ Server/Login.js
@@ -1,15 +1,40 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-mongoose.connect("mongodb://localhost:27017/")
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/ManagerStudent')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
 
-app.get("/", (req, res) =>{
-    res.render("./../User/User-Anothers/Login.html")
-})
+// Define a User schema
+const userSchema = new mongoose.Schema({
+    MSSV: String,
+    password: String
+});
 
-app.listen("3001", ()=>{
-    console.log("Server is running!!!");
-})
+const User = mongoose.model('User', userSchema);
+
+// Serve your HTML file
+app.use(express.static('public'));
+
+// Endpoint to handle login
+app.post('/User/User-Anothers/Login.html', async (req, res) => {
+    const { MSSV, password } = req.body;
+
+    // Find the user in the database
+    const user = await User.findOne({ MSSV, password });
+    if (user) {
+        res.send('Login successful');
+    } else {
+        res.send('Invalid credentials');
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
