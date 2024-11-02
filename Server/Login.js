41fb07558eaeb4162509ec50df517946f9ee5b8d
Mybui11qwer <145487630+Mybui11qwer@users.noreply.github.com>
2024-11-02 13:50:39 +0700

    Update 2/10

--- Server/Login.js
+++ Server/Login.js
@@ -6,31 +6,32 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/ManagerStudent')
+mongoose.connect('mongodb://localhost:27017/ManagerUser')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
 // Define a User schema
 const userSchema = new mongoose.Schema({
     MSSV: String,
-    password: String
+    Password: String
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('Student', userSchema);
 
-// Serve your HTML file
-app.use(express.static('public'));
+// Server your HTML file
+app.use(express.static('User'));
 
 // Endpoint to handle login
 app.post('/User/User-Anothers/Login.html', async (req, res) => {
-    const { MSSV, password } = req.body;
-
+    const { MSSV, Password } = req.body;
     // Find the user in the database
-    const user = await User.findOne({ MSSV, password });
+    const user = await User.findOne({ MSSV, Password });
     if (user) {
-        res.send('Login successful');
+        console.log('Đăng nhập thành công');
+        return res.redirect('User/User-interface-main/Notification.html');
     } else {
-        res.send('Invalid credentials');
+        console.log('Login failed: User not found');
+        return res.send('Không tồn tại tài khoản');
     }
 });
 
